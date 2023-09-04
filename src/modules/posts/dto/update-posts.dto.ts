a23ff13927e6b5b8ad671c3ac1678d7173a5aa9a
Vien <vienhoanglong789@gmail.com>
2023-09-04 13:32:09 +0700

    chore: update field reasonBan for update posts

--- src/modules/posts/dto/update-posts.dto.ts
+++ src/modules/posts/dto/update-posts.dto.ts
@@ -1,8 +1,10 @@
-import { OmitType, PartialType } from '@nestjs/swagger';
+import { ApiPropertyOptional, OmitType, PartialType } from '@nestjs/swagger';
 import { CreatePostsDto } from './create-posts.dto';
 
 export class UpdatePostsDto extends PartialType(
   OmitType(CreatePostsDto, ['createdBy']),
 ) {
   thumbnailName: string;
+  @ApiPropertyOptional({ type: String })
+  reasonBan: string;
 }
